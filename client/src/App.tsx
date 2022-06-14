697f97b45e5f158a26a24706be659970e341ce22
huytu20 <danghuytu.thd2006@gmail.com>
2022-06-14 22:20:08 +0700

    set up

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+const App = () => {
+
+  return (
+    <div className="container">
+      hfbhdsfbhds
+    </div>
+  )
+}
+
+export default App
+
