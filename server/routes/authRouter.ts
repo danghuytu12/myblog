fdf99f0dc76b1b9f75612994a7d5ae21f5ecb978
huytu20 <danghuytu.thd2006@gmail.com>
2022-06-15 23:40:31 +0700

    user server

---
+++ server/routes/authRouter.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import authCtrl from '../controllers/authCtrl'
+
+
+const router = express.Router()
+
+router.post('/register', authCtrl.register)
+
+
+export default router;
