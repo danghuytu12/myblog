2b46b76f866b67ffe6555ee3bed2e139640d40a1
huytu20 <danghuytu.thd2006@gmail.com>
2022-06-16 23:07:57 +0700

    register server

--- server/routes/authRouter.ts
+++ server/routes/authRouter.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 import authCtrl from '../controllers/authCtrl'
-
+import { validRegister } from "../middleware/vaild";
 
 const router = express.Router()
 
-router.post('/register', authCtrl.register)
+router.post('/register',validRegister, authCtrl.register)
 
 
 export default router;
